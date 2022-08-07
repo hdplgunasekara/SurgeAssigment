1e52eded11f048c132de4abf87dc2705ef49efac
Pasindu Lakshan Gunasekara <66919299+hdplgunasekara@users.noreply.github.com>
2022-08-07 18:18:50 +0530

    create login validation and navbar
    create login validation and navbar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 
 
@@ -7,27 +9,33 @@ const Navbar = () => {
     return (
         <div>
 
-<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark ">
   <div class="container-fluid">
-    <a class="navbar-brand" href="javascript:void(0)">Logo</a>
+    <a class="navbar-brand" href="javascript:void(0)">SURGE ASSIGNMENT</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="mynavbar">
       <ul class="navbar-nav me-auto">
         <li class="nav-item">
-          <a class="nav-link" href="javascript:void(0)">Link</a>
+          <a class="nav-link"  href="/login">Login</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="javascript:void(0)">Link</a>
+          <a class="nav-link" href="/adduser">Add User</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="javascript:void(0)">Link</a>
+          <a class="nav-link" href="/userlist">View User</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/addnote">Add Note</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/notelist">View Notes</a>
         </li>
       </ul>
       <form class="d-flex">
-        <input class="form-control me-2" type="text" placeholder="Search"/>
-        <button class="btn btn-primary" type="button">Search</button>
+       
+        <button class="btn btn-primary" type="button">Logout</button>
       </form>
     </div>
   </div>
--- frontend/src/pages/main/Login/index.jsx
+++ frontend/src/pages/main/Login/index.jsx
@@ -1,15 +1,67 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useState } from "react";
 import { Link} from "react-router-dom";
 import  "./login.styles.css";
+import UserContext from "../../../context/UserContext";
+import axios from "axios";
 
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  
+  const handleChangeEmail = (event) => {
+    setEmail(event.target.value);  
+  
+  }
+  const handleChangePassword = (event) => {
+    setPassword(event.target.value);  
+
+  }
+
+
+  const handleLogin = async (e) => {
+		e.preventDefault();
+		try {
+			const url = "http://localhost:8090/user/login";
+			const { data: res } = await axios.post(url, {email:email,password:password});
+			localStorage.setItem("accesstoken", res.accesstoken);
+      localStorage.setItem("refreshtoken", res.refreshtoken);
+      localStorage.setItem("id", res.id);
+      localStorage.setItem("email", res.email);
+      
+      if(!res.status){
+        window.location = "/adduser";
+      }else{
+      
+      if(res.permissionlevel==='Student'){
+        
+        window.location = "/notelist";
+      }
+
+      if(res.permissionlevel==='Admin'){
+        
+        window.location = "/userlist";
+      }
+    }
+
+		} catch (error) {
+			if (
+				error.response &&
+				error.response.status >= 400 &&
+				error.response.status <= 500
+			) {
+				alert(error.response.data.message);
+			}
+		}
+	};
+  
    
     return (
 
       <div className="Auth-form-container">
-        <form className="Auth-form">
+        <form className="Auth-form" onSubmit={handleLogin}>
           <div className="Auth-form-content">
             <h3 className="Auth-form-title">Sign In</h3>
             <div className="form-group mt-3">
@@ -18,6 +70,8 @@ const Login = () => {
                 type="email"
                 className="form-control mt-1"
                 placeholder="Enter email"
+                onChange={handleChangeEmail}
+                required
               />
             </div>
             <div className="form-group mt-3">
@@ -26,6 +80,8 @@ const Login = () => {
                 type="password"
                 className="form-control mt-1"
                 placeholder="Enter password"
+                onChange={handleChangePassword}
+                required
               />
             </div>
             <div className="d-grid gap-2 mt-3">
